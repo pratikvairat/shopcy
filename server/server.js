4b7e391095c387f6f379248f334ec6998c989e1c
Pratik Vairat <pratikvairat30@gmail.com>
2023-06-13 15:13:12 +0530

    SERVER CONFIGURE

--- server/server.js
+++ server/server.js
@@ -1,4 +1,12 @@
 const mysql=require("mysql2");
+const express=require('express');
+const cors=require('cors');
+const bodyParser=require('body-parser');
+const app=express();
+app.use(cors());
+const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const pool=mysql.createConnection({
     host: 'localhost',
